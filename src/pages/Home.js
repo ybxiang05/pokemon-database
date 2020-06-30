5e9349fd25ed33ea8056d869a6b9c17d3fd40d85
Brooke <brooke.xiang@gmail.com>
2020-06-30 11:31:10 -0700

    use state hook to work with filter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Header from "../components/Header";
 import CardGrid from "../components/CardGrid";
+import FilterCard from "../components/filterCard";
+import useFilter from "../hooks/useFilter";
 
 const Home = props => {
   const { data } = props;
-  console.log(data);
+  const [searchInput, setSearchInput] = React.useState("");
+  const filteredPokemon = useFilter(data, searchInput);
+  console.log(filteredPokemon);
   return (
     <>
       <Header />
-      <CardGrid data={data} />
+      <FilterCard data={data} searchInput={searchInput} setSearchInput={setSearchInput} />
+      <CardGrid data={filteredPokemon} />
     </>
   );
 };
